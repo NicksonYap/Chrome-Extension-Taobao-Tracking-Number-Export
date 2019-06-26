f205ce1b1395d14252466e9d023c56cb558fc875
Nickson Yap <hi@nickson.me>
2019-06-26 16:54:36 +0800

    add prettier config and format

--- content_script.js
+++ content_script.js
@@ -1,152 +1,138 @@
 $(document).ready(function() {
+  //ref: http://stackoverflow.com/a/1199420/3553367
+  String.prototype.trunc =
+    String.prototype.trunc ||
+    function(n) {
+      return this.length > n ? this.substr(0, n - 1) + '&hellip;' : this;
+    };
 
-//ref: http://stackoverflow.com/a/1199420/3553367
-String.prototype.trunc = String.prototype.trunc ||
-      function(n){
-          return (this.length > n) ? this.substr(0, n-1) + '&hellip;' : this;
-      };
+  var trackingCompleted = 0;
+  var trackingTotal = 0;
 
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      //document.getElementById("demo").innerHTML = xhttp.responseText;
+      //console.log(xhttp.responseText);
+      var returnedJSON = JSON.parse(xhttp.responseText);
 
-    var trackingCompleted = 0;
-    var trackingTotal = 0;
+      var orderList = [];
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-            //document.getElementById("demo").innerHTML = xhttp.responseText;
-            //console.log(xhttp.responseText);
-            var returnedJSON = JSON.parse(xhttp.responseText);
+      console.log(returnedJSON.mainOrders);
 
-            var orderList = [];
+      var count = 0;
 
-            console.log(returnedJSON.mainOrders);
+      var filteredMainOrders = [];
 
-            var count = 0;
+      returnedJSON.mainOrders.forEach(function(order) {
+        var excludeOrder = false;
+        var orderTime = moment(order.orderInfo.createTime);
+        var startTime = moment('2017-05-14');
 
-
-            var filteredMainOrders = [];
-
-            returnedJSON.mainOrders.forEach(function(order) {
-                var excludeOrder = false;
-                var orderTime = moment(order.orderInfo.createTime);
-                var startTime = moment("2017-05-14");
-
-                //console.log(orderTime);
-                if(!orderTime.isAfter(startTime)){
-                	excludeOrder = true;
-                }
-                /*if(order.statusInfo.text != "交易成功"){
+        //console.log(orderTime);
+        if (!orderTime.isAfter(startTime)) {
+          excludeOrder = true;
+        }
+        /*if(order.statusInfo.text != "交易成功"){
                 	excludeOrder = true;
                 }*/
 
-                if (!excludeOrder) {
-                    trackingTotal++;
-                    filteredMainOrders.push(order);
+        if (!excludeOrder) {
+          trackingTotal++;
+          filteredMainOrders.push(order);
+        }
+      });
+
+      console.log('to be tracked', trackingTotal);
+
+      filteredMainOrders.forEach(function(order) {
+        count++;
+        var orderItem = {};
+        // orderItem.id = order.id;
+        orderItem.no = count;
+        orderItem.time = order.orderInfo.createTime;
+        orderItem.shipper = '';
+        orderItem.trackingNum;
+        orderItem.trackingStat = '';
+        orderItem.title = '';
+
+        order.subOrders.forEach(function(subOrder) {
+          //orderItem.title += subOrder.itemInfo.title.substring(0, 20) + ";";
+          orderItem.title += subOrder.itemInfo.title.trunc(20) + '; ';
+        });
+        orderItem.title.trunc(50);
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (xhttp.readyState == 4 && xhttp.status == 200) {
+            try {
+              //document.getElementById("demo").innerHTML = xhttp.responseText;
+              var returnedJSON = JSON.parse(xhttp.responseText);
+              console.log(order.id, returnedJSON);
+
+              if (returnedJSON.isSuccess == true || returnedJSON.isSuccess == 'true') {
+                orderItem.shipper = returnedJSON.expressName;
+                orderItem.trackingNum = returnedJSON.expressId;
+                if (returnedJSON.address && returnedJSON.address[0]) {
+                  orderItem.trackingStat = returnedJSON.address[0].place.trunc(20);
+                } else {
+                  orderItem.trackingStat = 'NONE';
                 }
-            })
+              } else {
+                orderItem.shipper = '-';
+                orderItem.trackingNum = '-';
+                orderItem.trackingStat = '-';
+                console.log(order.id, 'no tracking or cancelled order');
+              }
 
-            console.log("to be tracked", trackingTotal);
-
-            filteredMainOrders.forEach(function(order) {
-                count++;
-                var orderItem = {};
-                // orderItem.id = order.id;
-                orderItem.no = count;
-                orderItem.time = order.orderInfo.createTime;
-                orderItem.shipper = "";
-                orderItem.trackingNum;
-                orderItem.trackingStat = "";
-                orderItem.title = "";
-
-                order.subOrders.forEach(function(subOrder) {
-                    //orderItem.title += subOrder.itemInfo.title.substring(0, 20) + ";";
-                    orderItem.title += subOrder.itemInfo.title.trunc(20) + "; ";
-                })
-				orderItem.title.trunc(50);
-
-
-                var xhttp = new XMLHttpRequest();
-                xhttp.onreadystatechange = function() {
-                    if (xhttp.readyState == 4 && xhttp.status == 200) {
-
-
-                        try {
-                            //document.getElementById("demo").innerHTML = xhttp.responseText;
-                            var returnedJSON = JSON.parse(xhttp.responseText);
-                            console.log(order.id, returnedJSON);
-
-                            if (returnedJSON.isSuccess == true || returnedJSON.isSuccess == 'true') {
-                                orderItem.shipper = returnedJSON.expressName;
-                                orderItem.trackingNum = returnedJSON.expressId;
-                                if (returnedJSON.address && returnedJSON.address[0]) {
-                                    orderItem.trackingStat = returnedJSON.address[0].place.trunc(20);
-                                } else {
-                                    orderItem.trackingStat = 'NONE';
-                                }
-                            } else {
-                                orderItem.shipper = '-';
-                                orderItem.trackingNum = '-';
-                                orderItem.trackingStat = '-';
-                                console.log(order.id, "no tracking or cancelled order");
-                            }
-
-                            //console.log(returnedJSON);
-                            //console.log(orderItem.time, returnedJSON.expressName, returnedJSON.expressId, orderItem.title);
-                            /* orderItem.shipper = returnedJSON.expressName;
+              //console.log(returnedJSON);
+              //console.log(orderItem.time, returnedJSON.expressName, returnedJSON.expressId, orderItem.title);
+              /* orderItem.shipper = returnedJSON.expressName;
                              orderItem.trackingNum = returnedJSON.expressId;
                              orderItem.trackingStat = returnedJSON.address[0].place;*/
+            } catch (e) {
+              orderItem.shipper = '-';
+              orderItem.trackingNum = '-';
+              orderItem.trackingStat = '-';
+              //console.log(order.id, "no tracking");
+              console.log(order.id, 'not json');
+            }
 
-                        } catch (e) {
-                            orderItem.shipper = '-';
-                            orderItem.trackingNum = '-';
-                            orderItem.trackingStat = '-';
-                            //console.log(order.id, "no tracking");
-                            console.log(order.id, "not json");
-                        }
+            orderList.push(orderItem);
 
-                        orderList.push(orderItem);
+            //console.log(orderItem);
 
+            trackingCompleted++;
 
-                        //console.log(orderItem);
+            //if tracking completed
+            if (trackingCompleted >= trackingTotal) {
+              orderList.sort(function(a, b) {
+                return a.no - b.no;
+              });
 
-                        trackingCompleted++;
+              console.log(orderList);
 
-                        //if tracking completed
-                        if (trackingCompleted >= trackingTotal) {
-                            orderList.sort(function(a, b) {
-                                return a.no - b.no
-                            });
+              chrome.runtime.sendMessage({ action: 'orderList', orderList: orderList }, function(response) {
+                console.log(response.farewell);
+              });
+            }
+          }
+        };
+        //var postURL = "https://buyertrade.taobao.com/trade/json/transit_step.do?bizOrderId=" + order.id;
+        // var postURL = "https://detail.i56.taobao.com/call/queryTrace.do?dimension=TRADE_ID&tradeId=" + order.id;
+        var postURL = 'https://buyertrade.taobao.com/trade/json/transit_step.do?bizOrderId=' + order.id;
+        xhttp.open('get', postURL, true);
+        //console.log(postURL);
+        xhttp.send();
+      });
 
-                            console.log(orderList);
-
-                            chrome.runtime.sendMessage({ action: "orderList", orderList: orderList }, function(response) {
-                                console.log(response.farewell);
-                            });
-
-                        }
-                    }
-                };
-                //var postURL = "https://buyertrade.taobao.com/trade/json/transit_step.do?bizOrderId=" + order.id;
-                // var postURL = "https://detail.i56.taobao.com/call/queryTrace.do?dimension=TRADE_ID&tradeId=" + order.id;
-                var postURL = "https://buyertrade.taobao.com/trade/json/transit_step.do?bizOrderId=" + order.id;
-                xhttp.open("get", postURL, true);
-                //console.log(postURL);
-                xhttp.send();
-
-
-
-
-            })
-
-
-            // console.log(tradeIDs);
-
-        }
-    };
-    var postURL = "//buyertrade.taobao.com/trade/itemlist/asyncBought.htm?action=itemlist/BoughtQueryAction&event_submit_do_query=1&_input_charset=utf8";
-    var params = "pageNum=1&pageSize=100";
-    xhttp.open("post", postURL, true);
-    xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    console.log(postURL, params);
-    xhttp.send(params);
+      // console.log(tradeIDs);
+    }
+  };
+  var postURL = '//buyertrade.taobao.com/trade/itemlist/asyncBought.htm?action=itemlist/BoughtQueryAction&event_submit_do_query=1&_input_charset=utf8';
+  var params = 'pageNum=1&pageSize=100';
+  xhttp.open('post', postURL, true);
+  xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  console.log(postURL, params);
+  xhttp.send(params);
 });
--- popup.js
+++ popup.js
@@ -1,112 +1,108 @@
-const laravelDomain = "https://laravel-purchase-pool-nicksonyap-3.c9users.io/" //must have '/' character at the end
-
-
+const laravelDomain = 'https://laravel-purchase-pool-nicksonyap-3.c9users.io/'; //must have '/' character at the end
 
 function onWindowLoad() {
+  chrome.tabs.executeScript(
+    null,
+    {
+      file: 'jquery-2.2.2.min.js'
+    },
+    function() {
+      chrome.tabs.executeScript(
+        null,
+        {
+          file: 'moment.js'
+        },
+        function() {
+          chrome.tabs.executeScript(
+            null,
+            {
+              file: 'content_script.js'
+            },
+            function() {
+              chrome.cookies.get(
+                {
+                  url: laravelDomain + 'taobao/order/add',
+                  name: 'XSRF-TOKEN'
+                },
+                function(cookie) {
+                  var laravelXSRFToken = cookie.value;
 
-
-
-    chrome.tabs.executeScript(null, {
-        file: "jquery-2.2.2.min.js"
-    }, function() {
-        chrome.tabs.executeScript(null, {
-            file: "moment.js"
-        }, function() {
-            chrome.tabs.executeScript(null, {
-                file: "content_script.js"
-            }, function() {
-
-
-                chrome.cookies.get({
-                    "url": laravelDomain + "taobao/order/add",
-                    "name": 'XSRF-TOKEN'
-                }, function(cookie) {
-                    var laravelXSRFToken = cookie.value;
-
-                    chrome.tabs.query({
-                        active: true,
-                        currentWindow: true
-                    }, function(tabs) {
-                        console.log(tabs[0].id);
-                        chrome.tabs.sendMessage(tabs[0].id, {
-                            action: "laravelXSRFToken",
-                            source: laravelXSRFToken
-                        });
-                        console.log(laravelXSRFToken);
-                    });
-
-
-
-                });
-
-            });
-        });
-    });
-
-
-
-
+                  chrome.tabs.query(
+                    {
+                      active: true,
+                      currentWindow: true
+                    },
+                    function(tabs) {
+                      console.log(tabs[0].id);
+                      chrome.tabs.sendMessage(tabs[0].id, {
+                        action: 'laravelXSRFToken',
+                        source: laravelXSRFToken
+                      });
+                      console.log(laravelXSRFToken);
+                    }
+                  );
+                }
+              );
+            }
+          );
+        }
+      );
+    }
+  );
 }
 
 var count = 0;
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log(request);
-    if (request.action == "getAllItemAndTrackingInfo") {
+  console.log(request);
+  if (request.action == 'getAllItemAndTrackingInfo') {
+    $('#message').remove();
 
-        $('#message').remove();
+    //paidListings[count][0] //date
+    //paidListings[count][1]  //status
+    //paidListings[count][2] //tradeID
+    //paidListings[count][3]  //sellerID
+    //paidListings[count][4] //sellerName
+    //paidListings[count][5][0] = [] //array of item id
+    //paidListings[count][5][1] = [] //array of item names
+    //paidListings[count][5][2] = [] //array of item snapshot id
+    //paidListings[count][5][3] = [] //array of item price
+    //paidListings[count][5][4] = [] //array of item quantity
+    //paidListings[count][6]  //total cost
+    //paidListings[count][7]  // tracking number
+    //paidListings[count][8]  // shipment provider
 
-        //paidListings[count][0] //date
-        //paidListings[count][1]  //status
-        //paidListings[count][2] //tradeID
-        //paidListings[count][3]  //sellerID
-        //paidListings[count][4] //sellerName
-        //paidListings[count][5][0] = [] //array of item id
-        //paidListings[count][5][1] = [] //array of item names
-        //paidListings[count][5][2] = [] //array of item snapshot id
-        //paidListings[count][5][3] = [] //array of item price
-        //paidListings[count][5][4] = [] //array of item quantity
-        //paidListings[count][6]  //total cost
-        //paidListings[count][7]  // tracking number
-        //paidListings[count][8]  // shipment provider
-
-        count++;
-        $("#target").append('<table id="list"></table>');
-        $("#list").append('<tr id="' + count + '"></tr>');
-        $("tr#" + count).append("<td>" + count + "</td>");
-        $("tr#" + count).append("<td>" + request.source[8] + "</td>");
-        $("tr#" + count).append("<td>" + request.source[7] + "</td>");
-        $("tr#" + count).append("<td>" + request.source[6][1].toString().substring(0, 30) + "</td>");
-        /* $.each(request.source, function(index, value) {
+    count++;
+    $('#target').append('<table id="list"></table>');
+    $('#list').append('<tr id="' + count + '"></tr>');
+    $('tr#' + count).append('<td>' + count + '</td>');
+    $('tr#' + count).append('<td>' + request.source[8] + '</td>');
+    $('tr#' + count).append('<td>' + request.source[7] + '</td>');
+    $('tr#' + count).append('<td>' + request.source[6][1].toString().substring(0, 30) + '</td>');
+    /* $.each(request.source, function(index, value) {
              //$("#list").append("<li>" + (index + 1) + "&nbsp;"+ value[index][8] + "&nbsp;"+ value[index][7] + "&nbsp;" +value[index][5][1][0] + "</li>");
              $("#list").append("<li>" + (index + 1) + "&nbsp;"+ value[index][8] + "&nbsp;"+ value[index][7] +"</li>");
          });*/
 
+    //   message.innerText = request.source;
+  }
 
-        //   message.innerText = request.source;
-    }
+  if (request.action == 'orderList') {
+    sendResponse({ farewell: 'goodbye' });
 
-    if (request.action == "orderList") {
-        sendResponse({ farewell: "goodbye" });
+    $('#message').remove();
 
-        $('#message').remove();
+    $('#target').append('<table id="list"></table>');
 
-        $("#target").append('<table id="list"></table>');
-
-        request.orderList.forEach(function(order) {
-            $("#list").append('<tr id="' + order.no + '"></tr>');
-            $("tr#" + order.no).append("<td>" + order.no + "</td>");
-            $("tr#" + order.no).append("<td>" + order.shipper + "</td>");
-            $("tr#" + order.no).append("<td>" + "'" + order.trackingNum + "</td>");
-            $("tr#" + order.no).append("<td>" + order.title + "</td>");
-            $("tr#" + order.no).append("<td>" + order.time + "</td>");
-            $("tr#" + order.no).append("<td>" + order.trackingStat + "</td>");
-
-        })
-
-
-    }
-
+    request.orderList.forEach(function(order) {
+      $('#list').append('<tr id="' + order.no + '"></tr>');
+      $('tr#' + order.no).append('<td>' + order.no + '</td>');
+      $('tr#' + order.no).append('<td>' + order.shipper + '</td>');
+      $('tr#' + order.no).append('<td>' + "'" + order.trackingNum + '</td>');
+      $('tr#' + order.no).append('<td>' + order.title + '</td>');
+      $('tr#' + order.no).append('<td>' + order.time + '</td>');
+      $('tr#' + order.no).append('<td>' + order.trackingStat + '</td>');
+    });
+  }
 });
-
 
 window.onload = onWindowLoad;
